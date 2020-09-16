e0d25e278c6ed530eacaee89ad9894ab5cf7e883
Andrew Dimmer <contactandrewd@gmail.com>
2020-09-16 08:34:27 -0400

    Updated Live Site as a Placeholder before Single Source migration

--- static/js/live.js
+++ static/js/live.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyDR6LpGExH6Vg9qMMTNRlV2uM8GSG7wtcg",
-	authDomain: "mythirdagent-32541.firebaseapp.com",
-	databaseURL: "https://mythirdagent-32541.firebaseio.com",
-	projectId: "mythirdagent-32541",
-	storageBucket: "mythirdagent-32541.appspot.com",
-	messagingSenderId: "660038034431"
+  apiKey: "AIzaSyDR6LpGExH6Vg9qMMTNRlV2uM8GSG7wtcg",
+  authDomain: "mythirdagent-32541.firebaseapp.com",
+  databaseURL: "https://mythirdagent-32541.firebaseio.com",
+  projectId: "mythirdagent-32541",
+  storageBucket: "mythirdagent-32541.appspot.com",
+  messagingSenderId: "660038034431",
 };
 
 firebase.initializeApp(config);
@@ -15,37 +15,37 @@ const settings = { timestampsInSnapshots: true };
 db.settings(settings);
 
 /* Timer */
-var hackingStarts = new Date("Sep 28, 2019 12:00:00");
-var hackingEnds = new Date("Sep 29, 2019 12:00:00");
+var hackingStarts = new Date("Sep 18, 2020 19:00:00");
+var hackingEnds = new Date("Sep 20, 2020 10:00:00");
 
-var x = setInterval(function() {
-	var now = new Date().getTime();
-	var distance = hackingStarts.getTime() - now;
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = hackingStarts.getTime() - now;
 
-	if(now > hackingStarts) {
-		distance = now - hackingEnds.getTime();
-	}
-	
-	if(distance < 0)  {
-		distance *= -1;
-	}
+  if (now > hackingStarts) {
+    distance = now - hackingEnds.getTime();
+  }
 
-	var hours = Math.floor((distance / (1000 * 60 * 60)));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  if (distance < 0) {
+    distance *= -1;
+  }
 
-	var textModifier = "";
+  var hours = Math.floor(distance / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-	if(now < hackingStarts) {
-		textModifier = "until hacking starts."
-	} else if(now < hackingEnds && now > hackingStarts) {
-		textModifier = "until hacking ends."
-	} else if(now > hackingEnds) {
-		textModifier = "Hacking Ended!"
-	}
+  var textModifier = "";
 
-	$("#time").text(hours + ":" + minutes + ":" + seconds);
-	$("#timerAppendedText").text(textModifier);
+  if (now < hackingStarts) {
+    textModifier = "until opening ceremony starts.";
+  } else if (now < hackingEnds && now > hackingStarts) {
+    textModifier = "until hacking ends.";
+  } else if (now > hackingEnds) {
+    textModifier = "Hacking Ended!";
+  }
+
+  $("#time").text(hours + ":" + minutes + ":" + seconds);
+  $("#timerAppendedText").text(textModifier);
 }, 1000);
 
 /* Schedule */
@@ -57,32 +57,57 @@ db.collection("scheduleItems").orderBy("order").get().then((querySnapshot) => {
 });
 */
 function createScheduleItem(scheduleItem) {
-	return '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">' + 
-				'<div class="d-flex w-100 justify-content-between">' +
-					'<h5 class="mb-1">' + scheduleItem.title + '</h5>' +
-					'<small>' + scheduleItem.type + '</small>' +
-				'</div>' +
-				'<p class="mb-1">Starts: ' + scheduleItem.start + '</p>' +
-				'<p class="mb-1">Ends: ' + scheduleItem.end + '</p>' +
-				'<p class="mb-1">Location: ' + scheduleItem.location + '</p>' +
-			'</a>';
+  return (
+    '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">' +
+    '<div class="d-flex w-100 justify-content-between">' +
+    '<h5 class="mb-1">' +
+    scheduleItem.title +
+    "</h5>" +
+    "<small>" +
+    scheduleItem.type +
+    "</small>" +
+    "</div>" +
+    '<p class="mb-1">Starts: ' +
+    scheduleItem.start +
+    "</p>" +
+    '<p class="mb-1">Ends: ' +
+    scheduleItem.end +
+    "</p>" +
+    '<p class="mb-1">Location: ' +
+    scheduleItem.location +
+    "</p>" +
+    "</a>"
+  );
 }
 
 /* Challenges */
-db.collection("challenges").get().then((querySnapshot) => {
-		querySnapshot.forEach((doc) => {
-			$("#challengeList").append(createChallenge(doc.data()));
-		});
-});
+db.collection("challenges")
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      $("#challengeList").append(createChallenge(doc.data()));
+    });
+  });
 
 function createChallenge(challenge) {
-	return '<a href="' + challenge.url + '" class="list-group-item list-group-item-action flex-column align-items-start">' + 
-				'<div class="d-flex w-100 justify-content-between">' +
-					'<h5 class="mb-1">' + challenge.title + '</h5>' +
-					'<small>' + challenge.sponsor + '</small>' +
-				'</div>' +
-				'<p class="mb-1">' + challenge.description + '</p>' +
-				'<small>' + challenge.type + ' Prize</small>' +
-			'</a>';
+  return (
+    '<a href="' +
+    challenge.url +
+    '" class="list-group-item list-group-item-action flex-column align-items-start">' +
+    '<div class="d-flex w-100 justify-content-between">' +
+    '<h5 class="mb-1">' +
+    challenge.title +
+    "</h5>" +
+    "<small>" +
+    challenge.sponsor +
+    "</small>" +
+    "</div>" +
+    '<p class="mb-1">' +
+    challenge.description +
+    "</p>" +
+    "<small>" +
+    challenge.type +
+    " Prize</small>" +
+    "</a>"
+  );
 }
-
